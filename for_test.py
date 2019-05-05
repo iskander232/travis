f412dc25268d454b21942c07be7eecd4e147b687
Iskander_Sagitov <iskander.s@bk.ru>
2019-05-05 14:40:31 +0300

    CI and tests

--- for_test.py
+++ for_test.py
@@ -1,8 +1,6 @@
 class C:
-    def __init__(self, input_file, output_file, key, model_file=None):
+    def __init__(self, input_file, output_file, key):
         self.input_file = input_file
         self.output_file = output_file
         self.key = key
-        self.model_file = model_file
-
 
